ac4988ccdbbbce176d9b5f00f22d5cc2697c4ac0
Henrique Rodrigues <hsr@cs.ucsd.edu>
2013-08-05 22:10:50 -0700

    Adding two new mininet topologies to the repository (for testing).

--- tools/python/mininet/chain2switches.py
+++ tools/python/mininet/chain2switches.py
@@ -27,44 +27,15 @@ def multilayer():
 
     s1  = net.addSwitch( 's1' )
     s2  = net.addSwitch( 's2' )
-    s2.dpid = ('11:11:00:00:00:00:00:02'.replace(':', ''))
 
-    s3  = net.addSwitch( 's3' )
-    s3.dpid = ('11:11:00:00:00:00:00:03'.replace(':', ''))
-    
-    
-    s4  = net.addSwitch( 's4' )
-    s5  = net.addSwitch( 's5' )
-    s5.dpid = ('11:11:00:00:00:00:00:05'.replace(':', ''))
-    
-    s6  = net.addSwitch( 's6' )
-    s6.dpid = ('11:11:00:00:00:00:00:06'.replace(':', ''))
-    
-    s7  = net.addSwitch( 's7' )
-
-
-    info( '*** Adding hosts\n' )
     h11 = net.addHost( 'h11', ip='10.0.0.11' )
-    h12 = net.addHost( 'h12', ip='10.0.0.12' )
-    h71 = net.addHost( 'h71', ip='10.0.0.71' )
+    h21 = net.addHost( 'h21', ip='10.0.0.21' )
     
     info( '*** Creating links\n' )
-    s1.linkTo(s2)
 
-    s2.linkTo(s3)
-    s2.linkTo(s6)
-    
-    s3.linkTo(s4)
-    
-    s4.linkTo(s5)
-    
-    s5.linkTo(s6)
-
-    s6.linkTo(s7)
-    
+    s1.linkTo( s2 )
     h11.linkTo( s1 )
-    h12.linkTo( s1 )
-    h71.linkTo( s7 )
+    h21.linkTo( s2 )
 
     info( '*** Starting network\n')
     net.start()
