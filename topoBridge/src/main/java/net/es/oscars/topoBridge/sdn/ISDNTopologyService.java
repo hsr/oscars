77bbfa4f5059f78fa8be9d5a010a15a30d7858b6
Henrique Rodrigues <hsr@cs.ucsd.edu>
2013-08-12 17:14:01 -0700

    Improving documentation of ISDNTopologyService.
    + New method getDomainName(), that retrieves the domain name from a TopologyServiceIdentifier.

--- topoBridge/src/main/java/net/es/oscars/topoBridge/sdn/ISDNTopologyService.java
+++ topoBridge/src/main/java/net/es/oscars/topoBridge/sdn/ISDNTopologyService.java
@@ -10,8 +10,16 @@ import org.jdom.Document;
  * 
  * This extension to OSCARS was developed with similar goals as OSCARS-SDNPSS
  * (http://github.com/hsr/oscars-sdnpss). The idea is to isolate OSCARS
- * reservation functionalities from network devices configurations, leaving the
- * later as a responsibility of an SDN Controller.
+ * reservation functionalities from network devices configurations/management,
+ * leaving the later as a responsibility of an SDN Controller.
+ * 
+ * This interface describes a service that fetches topology information from 
+ * an SDN Controller. This service is used by OSCARS's Topology Bridge module
+ * to retrieve and convert the topology maintained by an SDN Controller to a
+ * format expected by OSCARS.
+ * 
+ * Note: at this time it is assumed that a SDN Controller is responsible for
+ * a single domain. 
  * 
  * @author Henrique Rodrigues <hsr@cs.ucsd.edu>
  * 
@@ -22,4 +30,10 @@ public interface ISDNTopologyService {
 	 * @return A XML/NMWG formatted topology file
 	 */
 	public Document getTopology();	
+	
+	/**
+	 * Retrieves the domain name from the SDN topology service
+	 * @return String domain Name
+	 */
+	public String getDomainName();
 }
